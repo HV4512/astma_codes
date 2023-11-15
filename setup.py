90b9945c404fcc804904ffa9c4781d160948b45f
Harsh Vardhan <jobbykarn@gmail.com>
2023-11-15 20:47:21 +0530

    1

---
+++ setup.py
@@ -0,0 +1,12 @@
+import pathlib
+
+import setuptools
+
+setuptools.setup(
+    name="astma",
+    version="0.1.0",
+    description="astma codes",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url=""
+)
